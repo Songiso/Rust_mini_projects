7ef95a9588702e7266a9c8c9c9f65651c014e880
Songiso <songicooper@gmail.com>
2025-02-20 20:49:17 +0200

    Add files via upload

---
+++ minigrep/src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+    
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+}
