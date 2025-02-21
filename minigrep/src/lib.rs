aab136e0936fa94bf347b2eaf1039e54d5616069
Songiso <songicooper@gmail.com>
2025-02-21 16:59:51 +0200

    Update lib.rs

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,14 +1,13 @@
 use std::fs;
 use std::error::Error;
 
-
-
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     let contents: String = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+    
+    let result = search(&config.query, &contents);
+    for line in result{
+        println!("{line}"); 
+    }
     Ok(())
 }
 
@@ -27,5 +26,33 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
